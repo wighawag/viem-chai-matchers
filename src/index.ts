027f1fb8380db643ac249f2ed72ac525ac8aa71a
wighawag <wighawag@gmail.com>
2023-06-10 21:46:47 +0100

    add includeEvent

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {TransactionExecutionError} from 'viem';
+import {Abi, TransactionExecutionError, TransactionReceipt, decodeEventLog} from 'viem';
 import './types';
 
 export function viemChaiMatchers(chai: Chai.ChaiStatic, utils: Chai.ChaiUtils) {
@@ -14,5 +14,23 @@ export function viemChaiMatchers(chai: Chai.ChaiStatic, utils: Chai.ChaiUtils) {
 	});
 	Assertion.addMethod('reverted', function () {
 		new Assertion(this._obj).to.eventually.be.rejected.and.be.an.instanceOf(TransactionExecutionError);
+	});
+
+	Assertion.addMethod('includeEvent', function (abi: Abi, eventName: string) {
+		const receipt: TransactionReceipt = this._obj;
+		for (const log of receipt.logs) {
+			try {
+				const event = decodeEventLog({
+					abi: abi,
+					topics: log.topics,
+					eventName: eventName,
+					data: log.data,
+				});
+				if (event.eventName === eventName) {
+					return new Assertion(event);
+				}
+			} catch {}
+		}
+		throw new Error(`could not find any matching event for ${eventName}`);
 	});
 }
--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,6 @@ declare module Chai {
 	export interface Assertion {
 		revertedWith(error: string): PromisedAssertion;
 		reverted(): PromisedAssertion;
+		includeEvent(abi: any, eventName: string): Assertion; // TODO Abi ?
 	}
 }
